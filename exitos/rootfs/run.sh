a796d8fcde1f928dabfa0ce35444266e08b21dc4
Pol Cañadas Costa <polcanadascosta@gmail.com>
2026-04-10 10:35:45 +0200

    Install paho-mqtt and run server.py

--- exitos/rootfs/run.sh
+++ exitos/rootfs/run.sh
@@ -3,5 +3,6 @@
 echo "Creem la carpeta (/share/exitos/) si no existeix, aqui guardarem fitxers persistents."
 mkdir -p /share/exitos/
 
+pip3 install paho-mqtt --break-system-packages > /dev/null 2>&1
 echo "Starting server.py..."
 python3 server.py
