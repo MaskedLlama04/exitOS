42c7fcf0dcd67e2142c24888c1210322f6fa76c5
Pol Cañadas Costa <polcanadascosta@gmail.com>
2026-04-17 09:24:15 +0200

    Update run.sh to install more packages and start services
    Install additional Python packages and start OpenRemote service.

--- exitos/rootfs/run.sh
+++ exitos/rootfs/run.sh
@@ -3,6 +3,9 @@
 echo "Creem la carpeta (/share/exitos/) si no existeix, aqui guardarem fitxers persistents."
 mkdir -p /share/exitos/
 
-pip3 install paho-mqtt --break-system-packages > /dev/null 2>&1
+pip3 install paho-mqtt requests urllib3 --break-system-packages > /dev/null 2>&1
+echo "Iniciant el Service Bridge d'OpenRemote en segon pla..."
+python3 openremote_service.py &
+
 echo "Starting server.py..."
 python3 server.py
