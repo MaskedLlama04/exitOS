93be6dfcdbc1d2629d6c89d41a128fac80751ff7
Pol Cañadas Costa <polcanadascosta@gmail.com>
2026-04-30 10:38:54 +0200

    Install Python dependencies in run.sh
    Added installation of Python dependencies in run.sh.

--- exitos/rootfs/run.sh
+++ exitos/rootfs/run.sh
@@ -3,6 +3,9 @@
 echo "Creem la carpeta (/share/exitos/) si no existeix, aqui guardarem fitxers persistents."
 mkdir -p /share/exitos/
 
+echo "Instal·lant dependències de Python (paho-mqtt, requests)..."
+python3 -m pip install paho-mqtt requests urllib3
+
 echo "Iniciant la connexió amb Open Remote..."
 python3 openremote_service.py &
 
