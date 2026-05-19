be108d81848103339abe384ba7e972b68bd4a828
Pol Cañadas Costa <polcanadascosta@gmail.com>
2026-05-19 11:51:08 +0200

    Remove Python dependencies installation from run.sh
    Removed Python dependencies installation step from run.sh.

--- exitos/rootfs/run.sh
+++ exitos/rootfs/run.sh
@@ -3,9 +3,6 @@
 echo "Creem la carpeta (/share/exitos/) si no existeix, aqui guardarem fitxers persistents."
 mkdir -p /share/exitos/
 
-echo "Instal·lant dependències de Python (paho-mqtt, requests)..."
-python3 -m pip install paho-mqtt requests urllib3 --break-system-packages
-
 echo "Iniciant la connexió amb Open Remote..."
 python3 openremote_service.py &
 
